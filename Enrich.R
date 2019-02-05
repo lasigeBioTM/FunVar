617f8c14dac96ed7689aed75a639ad4f02b28d32
Muh-Asif <39635420+Muh-Asif@users.noreply.github.com>
2019-02-05 09:17:01 +0000

    Update Enrich.R

--- Enrich.R
+++ Enrich.R
@@ -1,7 +1,6 @@
 ############ Enrichment analysis using g:profiler tool#####################################
 ###################################################################################################
 # Reference for package: https://cran.r-project.org/web/packages/gProfileR/gProfileR.pdf
-# Supeertive reference: 'Impact of outdate gene annotations on pathway enrichment analysis 2016'; 'pathway and network analysis of cancer genomes'
 
 library(gProfileR)
 
@@ -10,10 +9,10 @@ genes < -"genes.CSV"
 QueryGenes < -read.csv(genes, header=T)
 
 QueryGeneVector < -as.vector(QueryGenes[1])
-head(QueryGeneVector)
+#head(QueryGeneVector)
 
 # Enrichment analysis
-#  correction_method= "fdr", here value can be "analytical": value from tools (more regid), "gSCS",
+#  correction_method= "fdr",  "analytical", "gSCS",
 # "fdr", "bonferroni".
 EnrichmentResults = gprofiler(QueryGeneVector, organism="hsapiens", correction_method="fdr",
                               src_filter="GO:BP", exclude_iea=T, evcodes=T,
